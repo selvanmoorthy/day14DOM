8ac609c142bac6720833075c88804355e5c0705a
selvanmoorthy <124973954+selvanmoorthy@users.noreply.github.com>
2023-03-14 20:35:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function foo(){
+    var res1=document.getElementById("firstname").value;
+    var res2=document.getElementById("middlename").value;
+    var res3=document.getElementById("lastname").value;
+    var res4=document.getElementById("email").value;
+    var res5=document.getElementById("pass").value;
+    console.log(res1,res2,res3,res4,res5);
+
+}
